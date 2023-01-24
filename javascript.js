7bc1ddddb5e14014d44c9ca30f9d1f6cf817d231
crab79 <supercrab79@outlook.com>
2023-01-24 16:56:04 +0800

    Finish computerSelection and  playerSelection

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+
 let randomNumber = Math.floor(Math.random() * 100) + 1;
 function getComputerChoice(randomNumber) {
     if (randomNumber <= 30) {
@@ -8,5 +9,27 @@ function getComputerChoice(randomNumber) {
         return "Paper";
     }
 }
-let computerSelection = '電腦出拳 = '+ getComputerChoice(randomNumber)
-console.log(computerSelection); //檢查過了！
+let computerSelection = getComputerChoice(randomNumber)
+
+
+let  playerSelection = window.prompt("Rock, Paper, or Scissors?");
+
+if (playerSelection.toUpperCase() == "ROCK") {
+    console.log('你的選擇是： '+playerSelection);
+}else if (playerSelection.toUpperCase() == "PAPER") {
+    console.log('你的選擇是： '+playerSelection);
+}else if (playerSelection.toUpperCase() == "SCISSORS") {
+console.log('你的選擇是： '+playerSelection);
+}else {
+    while (playerSelection.toUpperCase() != "ROCK" && playerSelection.toUpperCase() != "PAPER" && playerSelection.toUpperCase() != "SCISSORS") {
+        alert('Please type "Rock", "Paper", or "Scissors"');
+        playerSelection = window.prompt("Rock, Paper, or Scissors?");
+    }
+    console.log('你的選擇是： '+playerSelection);
+}
+console.log('電腦出拳 = '+computerSelection);
+
+        
+
+
+
